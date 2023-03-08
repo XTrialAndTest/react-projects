36f3d56f11111a54ac1fc23574a7b3e49bb6d04a
X_Trial_Test <1trialandtest@gmail.com>
2023-03-08 21:22:38 +0300

    birthday Card

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,28 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  let [name, setName] = useState(data);
+  console.log(name);
+  return (
+    <>
+      <div className='container section '>
+        <h1>BIRTHDAY REMINDER</h1>
+        {name.map(({ id, name, age, image }) => {
+          return (
+            <>
+              <div className='main'>
+                <div className='singleCard person ' key={id}>
+                  <img src={image} alt='' /> <h4>{name}</h4>
+                  <p>{age}</p>
+                </div>
+              </div>
+            </>
+          );
+        })}
+        <button onClick={() => setName('0 birthdays today')}>clear</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
